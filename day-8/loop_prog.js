3f5396a4c365f5ec7a5b20d4117174773537ce3b
PayalPatel03 <p6579113@gmail.com>
2026-07-16 12:04:33 +0530

    d

---
+++ day-8/loop_prog.js
@@ -0,0 +1,15 @@
+//print 1 t 10 using for loop
+// let str='';
+// for(let i=1;i<=10;i++){
+//     str+= i+' ';
+// }
+// console.log(str);
+
+//Print numbers 1 to 10 using while loop
+ let str='';
+ let i=1;
+ while(i<=10){
+     str += i+'  ';
+     i++;
+ }
+ console.log(str);
